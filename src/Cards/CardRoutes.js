ce61fd41d046edecdc61f6311113788dd7972c82
dylan-adams-webdev <dadams31@asu.edu>
2022-03-03 16:18:03 -0700

    rename card components for readability

---
+++ src/Cards/CardRoutes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import NotFound from '../Common/NotFound';
+import EditCard from './EditCard';
+import NewCard from './NewCard';
+
+export default function CardRoutes() {
+	const { path } = useRouteMatch();
+	console.log(path);
+	return (
+		<Switch>
+			<Route exact path={`${path}/new`}>
+				<NewCard />
+			</Route>
+
+			<Route exact path={`${path}/:cardId/edit`}>
+				<EditCard />
+			</Route>
+			
+			<Route>
+				<NotFound />
+			</Route>
+		</Switch>
+	);
+}
