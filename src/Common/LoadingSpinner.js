3dc077a508dad7ce16dc051a5ea5beef593dd5cd
dylan-adams-webdev <dadams31@asu.edu>
2022-03-03 16:19:10 -0700

    Add common functionalilty to commit

---
+++ src/Common/LoadingSpinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LoadingSpinner() {
+	return (
+		<div className='spinner-border m-5' role='status'>
+			<span className='sr-only'>Loading...</span>
+		</div>
+	);
+}
--- src/Common/NotFound.js
+++ src/Common/NotFound.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react';
 
 function NotFound() {
-  return (
-    <div className="NotFound">
-      <h1>Not Found</h1>
-    </div>
-  );
+	return (
+		<div className='text-center'>
+			<h1 className='mb-5'>Page Not Found</h1>
+			<img
+				className='img-fluid w-75'
+				src='/img/not-found.svg'
+				alt='page not found'
+			/>
+		</div>
+	);
 }
 
 export default NotFound;
