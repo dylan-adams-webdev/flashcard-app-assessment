f59ceb86a421191a4f7b0ac9cf183f3f46f99097
dylan-adams-webdev <dadams31@asu.edu>
2022-03-04 13:10:54 -0700

    add incomplete functionality for study subcomponents

--- src/Decks/Study.js
+++ src/Decks/Study.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import LoadingSpinner from '../Common/LoadingSpinner';
-import Breadcrumbs from './StudyComponents/Breadcrumbs';
 import Card from './StudyComponents/Card';
+import { useDecks } from '../Hooks/Hooks';
+import LoadingSpinner from '../Common/LoadingSpinner';
+import { useParams } from 'react-router-dom';
+import NoDeck from '../Common/NoDeck';
 
-export default function Study({ decks }) {
+export default function Study() {
+	const { isLoading, decks } = useDecks();
 	const params = useParams();
+
+	if (isLoading) {
+		return <LoadingSpinner />;
+	}
+
 	const deck = decks.find((deck) => deck.id === Number(params.deckId));
-	
-	
-	if (deck) return (
-		<>
-			<Breadcrumbs deck={deck} />
-			<h1>{deck.name}: Study</h1>
-			<Card deck={deck}/>
-		</>
-	);
-	return <LoadingSpinner />;
+	if (deck)
+		return (
+			<>
+				<Card content={decks} />
+			</>
+		);
+	return <NoDeck />;
 }
