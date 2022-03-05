1d19fe5a2d325b1b89a3ba32e2e4f579f923e8a8
dylan-adams-webdev <dadams31@asu.edu>
2022-03-04 23:56:18 -0700

    createdeck in finished state with submission

--- src/Decks/CreateDeck.js
+++ src/Decks/CreateDeck.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Breadcrumbs from './DeckFormComponents/Breadcrumbs';
+import DeckForm from './DeckFormComponents/DeckForm';
+import { createDeck } from '../utils/api/index';
 
 export default function CreateDeck() {
+	const history = useHistory();
+	const submitHandler = (data) => {
+		createDeck(data).then((res) => history.push(`/decks/${res.id}`))
+	};
 	return (
-		<div>CreateDeck</div>
+		<>
+			<Breadcrumbs />
+			<h1>Create Deck</h1>
+			<DeckForm submitHandler={submitHandler} />
+		</>
 	);
 }
