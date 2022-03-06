3ee49345e13857718fd9ab6961323b541d5df621
dylan-adams-webdev <dadams31@asu.edu>
2022-03-05 19:36:48 -0700

    complete functionality of editdeck component

--- src/Decks/EditDeck.js
+++ src/Decks/EditDeck.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useDecks } from '../Hooks/Hooks';
+import DeckForm from './DeckFormComponents/DeckForm';
+import Breadcrumbs from '../Common/Breadcrumbs';
+import LoadingSpinner from '../Common/LoadingSpinner';
+import { updateDeck } from '../utils/api/index';
+import EmptyWithMsg from '../Common/EmptyWithMsg';
 
 export default function EditDeck() {
-	return <div>EditDeck</div>;
+	const { deckId } = useParams();
+	const history = useHistory();
+	const { decks, isLoading } = useDecks();
+	if (!isLoading) {
+		const deck = decks.find((item) => item.id === Number(deckId));
+		if(!deck) return <EmptyWithMsg msg="We can't find that deck..." />;
+		const breadCrumbAddresses = [
+			{
+				name: deck.name,
+				address: `/decks/${deck.id}`,
+			},
+			{ name: 'Edit Deck' },
+		];
+		const initialState = { name: deck.name, description: deck.description };
+		const submitHandler = (data) => {
+			const newDeck = {
+				...deck,
+				name: data.name,
+				description: data.description,
+			};
+			updateDeck(newDeck).then(history.push(`/decks/${deck.id}`));
+		};
+		return (
+			<>
+				<Breadcrumbs links={breadCrumbAddresses} />
+				<h1>Edit Deck</h1>
+				<DeckForm
+					submitHandler={submitHandler}
+					initialState={initialState}
+				/>
+			</>
+		);
+	}
+	else return <LoadingSpinner />;
+	
 }
