f8f162e93a56585e36a88934d79594fda242a534
dylan-adams-webdev <dadams31@asu.edu>
2022-03-05 19:38:25 -0700

    add nav buttons to error page

--- src/Common/EmptyWithMsg.js
+++ src/Common/EmptyWithMsg.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function EmptyWithMsg({ msg }) {
 	return (
 		<>
-			<div className='text-center'>
-				<h1 className='mb-5'>{msg}</h1>
+			<div>
+				<h1>{msg}</h1>
+				<Link to={'/'} className='btn btn-secondary mr-2'>
+					<i className='fa-solid fa-list'></i>&nbsp; List Decks
+				</Link>
+				<Link to={'/decks/new'} className='btn btn-primary'>
+					<i className='fa-solid fa-pencil'></i>&nbsp; Create Deck
+				</Link>
 				<img
-					className='img-fluid w-75'
+					className='img-fluid w-75 d-block mt-5'
 					src='/img/not-found.svg'
-					alt='page not found'
+					alt='deck not found'
 				/>
 			</div>
 		</>
