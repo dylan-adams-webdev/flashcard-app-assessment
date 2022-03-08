d3489ac447345a92018c7e42dea23c73593f554b
dylan-adams-webdev <dadams31@asu.edu>
2022-03-08 12:55:10 -0700

    disable submit button while form is submitting

--- src/Common/DynamicSubmitFormButton.js
+++ src/Common/DynamicSubmitFormButton.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 export default function DynamicDeckFormButton({ isSubmitting }) {
 	return (
-		<button type='submit' className='btn btn-primary'>
-			{isSubmitting ? (
-				<>
-					<span
-						className='spinner-border spinner-border-sm'
-						role='status'
-						aria-hidden='true'
-					></span>
-					&nbsp; loading...
-				</>
-			) : (
-				'Submit'
-			)}
-		</button>
-	);
+
+		<button type='submit' className={`btn btn-primary ${isSubmitting ? 'disabled' : ''
+			}`}>
+				{isSubmitting ? (
+					<>
+						<span
+							className='spinner-border spinner-border-sm'
+							role='status'
+							aria-hidden='true'
+						></span>
+						&nbsp; Loading...
+					</>
+				) : (
+					'Submit'
+				)}
+			</button>
+	)
 }
