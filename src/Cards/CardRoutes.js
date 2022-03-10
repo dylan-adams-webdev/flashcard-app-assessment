e2b5a27a89d3acffb4a1149f0f9c8011d21fb9b3
dylan-adams-webdev <dadams31@asu.edu>
2022-03-10 15:29:54 -0700

    Add refined error pages and finishing submission touches to all files

--- src/Cards/CardRoutes.js
+++ src/Cards/CardRoutes.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Switch, useRouteMatch, useParams } from 'react-router-dom';
+import LoadingSpinner from '../Common/LoadingSpinner';
 import NotFound from '../Common/NotFound';
+import GenericError from '../Common/NotFound';
+import CannotFindDeck from '../Common/CannotFindDeck';
 import { readDeck } from '../utils/api';
 import EditCard from './EditCard';
 import NewCard from './NewCard';
@@ -9,12 +12,22 @@ export default function CardRoutes() {
 	const { path } = useRouteMatch();
 	const { deckId } = useParams();
 	const [deck, setDeck] = useState(null);
+	const [error, setError] = useState(null);
 	
 	useEffect(() => {
 		const abort = new AbortController();
-		readDeck(deckId, abort.signal).then(setDeck);
+		readDeck(deckId, abort.signal).then(setDeck).catch(setError);
 		return () => abort.abort();
 	}, [deckId]);
+	
+	if(!deck && !error) return <LoadingSpinner />
+	if (error) {
+		if (error.message.includes('404')) {
+			return <CannotFindDeck />;
+		} else {
+			return <GenericError msg={'There was an anomaly...'} />;
+		}
+	}
 	
 	return (
 		<Switch>
--- src/Common/NotFound.js
+++ src/Common/NotFound.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function NotFound() {
 	return (
-		<div className='text-center'>
-			<h1>Not Found</h1>
-		</div>
+		<>
+			<span className='sr-only'>Not Found</span>
+			<h1>Page Not Found</h1>
+			<img className='img-fluid w-50' alt='Page not found' src='/img/not-found.svg' />
+		</>
 	);
 }
 
